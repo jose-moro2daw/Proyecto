6eba5a821cbd5fcf39d46371163399c9d2dac97f
jose-moro2daw <jose.moro.luque.alu@iesjulioverne.es>
2022-03-23 15:16:24 +0100

    Prueba

--- example-app/app/Http/Controllers/UserController.php
+++ example-app/app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Redirect,Response,File;
 use Illuminate\Support\Facades\Hash;
  
 class UserController extends Controller
--- registration-app/src/App.js
+++ registration-app/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route,Routes,withRouter} from "react-router-dom";
-import Login from "./IniciarSesion";
-import Signup from "./Registro";
-import Home from "./Home";
+import Login from "./components/IniciarSesion";
+import Signup from "./components/Registro";
+import Home from "./components/Home";
+import Header from "./components/Header";
  
 function App() {
   return (
     <Router>
+      <Header/>
     <div className="App">
       <Routes>
         <Route exact path="/Home" element={<Home/>} />
